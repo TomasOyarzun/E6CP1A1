281a6d18661165c942ddb1c683683c1c3d218d16
Tomas <oyarzun.tomas@gmail.com>
2018-06-22 14:04:26 -0400

    Finished section 'ciclos'

--- 3 ciclos anidados/ejercicio2.rb
+++ 3 ciclos anidados/ejercicio2.rb
@@ -24,3 +24,24 @@ Generar el código para imprimir la siguiente tabla:
   <tbody>
  </table>
 =end
+
+a = ''
+1.times do |i|
+  a += "<table>\n"
+  a += "<tbody>\n"
+  #Ciclo para los tr
+  for x in 1..3
+    a += " <tr>\n"
+    #Ciclo para los td
+    for z in 1..4
+      a += "   <td> #{z} </td>\n" if x == 1
+      a += "   <td> #{z + 4} </td>\n" if x == 2
+      a += "   <td> #{z + 7} </td>\n" if x == 3
+    end
+    a += " </tr>\n"
+  end
+  a += "</table>\n"
+  a += "</tbody>\n"
+end
+
+puts a
--- 3 ciclos anidados/ejercicio3.rb
+++ 3 ciclos anidados/ejercicio3.rb
@@ -2,3 +2,15 @@
 # la tabla de multiplicar del número ingresado. Debe repetir la operación hasta
 # que se ingrese un 0 (cero).
 # Ingrese un número (0 para salir): _
+
+puts 'Ingrese un número para conocer su tabla de multiplicar'
+num = gets.chomp.to_i
+
+#Ciclo para generar la tabla de multiplicar + validador
+while num != 0 do
+	for i in 1..10
+		puts "#{num} x #{i} = #{num * i}"
+	end
+	puts 'Ingrese un número para conocer su tabla de multiplicar'
+	num = gets.chomp.to_i
+end
